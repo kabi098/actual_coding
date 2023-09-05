99319dc06b3165fade088c2fd943275ae23091fe
Kabi <kawasthi098@gmail.com>
2023-09-05 14:34:27 +0545

    practice code

---
+++ practice.py
@@ -0,0 +1,49 @@
+name=input("What is your name\n")
+print(name)
+'''name=input("Enter your name: ")
+cast=input("Enter your cast: ")
+
+print("My full name is" , name, "raj ", end="",)
+print(cast)'''
+
+#raja=input("Enter your name: ")
+#print(f"HElLO, {raja}")
+
+'''name=input("Enter your name: ") .strip().lower()
+
+#name=name.strip()
+#name=name.title()
+#name=name.strip().title()
+
+
+
+print(f"Hello, {name}")'''
+'''a=int(input("Enter first number: "))
+b=int(input("Enter second number: "))
+
+add=a+b
+print(f"The sum of two number is {add}")'''
+
+'''a=input("Enter first number: ")
+b=input("Enter second number: ")
+
+add=a+b
+print(add)'''
+
+'''a=int(input("Enter first number: "))
+b=int(input("Enter second number: "))
+
+
+print(a+b)'''
+
+
+'''a=input("Enter first number: ")
+b=input("Enter second number: ")
+
+#add=int(a)+ int(b)
+#print(add)
+
+print("The sum is: ",int(a)+int(b))'''
+
+
+
